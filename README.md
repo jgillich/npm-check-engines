b966cd7160818785b1f40680f0deb1e0f1429cc5
Jakob Gillich <jakob@gillich.me>
2015-11-24 10:35:12 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# npm-check-engines
+
+Check that your dependencies support your supported engines.
+
+## Wait, what?
+
+You specify the supported engines in your [package.json](https://docs.npmjs.com/files/package.json#engines):
+
+```
+engines: {
+  node: '>= 4.0'
+}
+```
+
+If any of your dependencies do not support any version `>= 4.0` (4.0, 4.1, 4.2, 5.0 and so on),
+this script will tell you. It works by fetching a list of Node releases and then
+iterating over all dependencies.
---
+++ index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+var semver      = require('semver');
+var _           = require('lodash');
+var request     = require('request');
+var npm         = require('npm');
+var colors      = require('colors');
+var config      = require(process.cwd() + '/package.json');
+
+console.log(('checking ' + process.cwd() + '/package.json').green);
+
+npm.load(config, function (err, npm) {
+  handleErr(err);
+  npm.commands.ls([], true, function (err, root, lite) {
+    handleErr(err);
+    if (!root.engines || !root.engines.node) {
+      handleErr(new Error('engines.node is not defined'));
+    }
+
+    engines(root.engines.node, function (err, engines) {
+      handleErr(err);
+      if (!check(engines, root.dependencies)) {
+        console.log('no problems detected'.green);
+      } else {
+        process.exit(1);
+      }
+    });
+  });
+});
+
+function handleErr(err) {
+  if (err) {
+    console.error(err.message.red);
+    process.exit(1);
+  }
+}
+
+function engines(required, cb) {
+  request('http://nodejs.org/dist/', function (err, res, body) {
+    if (err) return cb(err);
+
+    var supported = [];
+    var regex = /([0-9]+\.[0-9]+\.[0-9]+)\/<\/a>/g;
+    var match;
+
+    while (match = regex.exec(body)) {
+      if (semver.satisfies(match[1], required)) {
+        supported.push(match[1]);
+      }
+    }
+    cb(null, supported);
+  });
+}
+
+function check(engines, packages, tree, fail) {
+  _.forEach(packages, function (pkg) {
+    if (pkg.missing) {
+      handleErr(new Error('missing package detected, please run npm install first'));
+    }
+    pkgTree = (tree || '') + pkg.name.red + ' ' + pkg.version;
+
+    if (pkg.engines && pkg.engines.node) {
+      engines.forEach(function (engine) {
+        if (!semver.satisfies(engine, pkg.engines.node)) {
+          console.error(pkgTree + ' does not satisfy engine ' +  engine.green + ' (' + pkg.engines.node.red + ')')
+          fail = true;
+        }
+      });
+    }
+    if (pkg.dependencies) {
+      fail = check(engines, pkg.dependencies, pkgTree  + ' > ', fail) || fail;
+    }
+  });
+  return !!fail;
+}
